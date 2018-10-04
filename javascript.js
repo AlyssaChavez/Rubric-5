c18371254d3ea0ff0902a4dfa86ee75e3c080ac7
Alyssa Chavez <alyssapie2328@gmail.com>
2018-10-03 23:23:14 -0500

    Updates

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
 // JavaScript Document
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function timing() {
+    alert('Hello');
+}
+
+  function ReadCookie()
+            {
+               var allcookies = document.cookie;
+               document.write ("All Cookies : " + allcookies );
+               
+         
+               cookiearray = allcookies.split(';');
+            
+               for(var i=0; i<cookiearray.length; i++){
+                  name = cookiearray[i].split('=')[0];
+                  value = cookiearray[i].split('=')[1];
+                  document.write ("Key is : 50" + name + " and Value is : 50" + 0);
+               }
+            }
+         //-->
